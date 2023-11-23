85194cab8a3d76218f2b8926d4ea4896bf38d69a
kcorbett95 <kevin.corbett95@gmail.com>
2023-11-22 16:24:08 -0800

    lab8 again

--- COMP/139/Lab 8 Harmonic Motion/HarmonicPlot.m
+++ COMP/139/Lab 8 Harmonic Motion/HarmonicPlot.m
@@ -5,7 +5,21 @@ clc
 %under = HarmonicMotion(10, 5, 10, 0.5, 0.5, 10, 1000);
 %over = HarmonicMotion(10,5,20,0.5,0.5,10,1000);
 %nodamping = HarmonicMotion(10, 5, 0, 0.5, 0.5, 10, 1000);
-zeta = [(10*sqrt(2)),10,20,0];
+% m = mass
+% k = spring constant
+% b = damping coefficient
+% x0 = initial displacement
+% v0 = initial velocity
+% tN = final time (initial time = 0)
+% N = number of points in time
+m = 10;
+k = 5;
+x0 = 0.5;
+v0 = 0.5;
+tN = 10;
+N = 1000;
+b = [(1/(2*sqrt(m*k))),(0.1/(8*sqrt(m*k))),(20/(sqrt(m*k))),0];
+
 %build overlayed plot
 figure(1);
 hold on;
@@ -15,7 +29,7 @@ xlabel("Time(s)");
 ylabel("Displacement");
 names = [];
 for i = 1:4  
-    [x,y,damping] = HarmonicMotion(10,5,(zeta(i)),0.5,0.5,10,1000);
+    [x,y,damping] = HarmonicMotion(m, k, b(i), x0, v0, tN, N);
     plot(y,x); 
     names = [names, damping];
 end
@@ -28,7 +42,7 @@ figure(2);
 for i = 1:4  
     subplot(4,1,i);
     
-    [x,y,damping] = HarmonicMotion(10,5,(zeta(i)),0.5,0.5,10,1000);
+    [x,y,damping] = HarmonicMotion(m, k, b(i), x0, v0, tN, N);
    plot(y,x);
    if i == 1
         title("Time-Domain Response of Damped Systems");
